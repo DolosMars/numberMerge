b5cc7b537d6036790ad6757662d4a41986cf0b67
DolosMars <high106huiyu@gmail.com>
2022-04-23 11:54:36 +0800

    Initial Commit

---
+++ numberMerge/numberMergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  numberMergeApp.swift
+//  numberMerge
+//
+//  Created by User06 on 2022/4/23.
+//
+
+import SwiftUI
+
+@main
+struct numberMergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
